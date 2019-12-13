049600591129306e881f8f66d91896b6f5a24c6f
ganeshphadtare87 <phadtare_ganesh@yahoo.com>
2019-12-12 17:49:16 -0800

    Changes added

--- kb_api/Startup.cs
+++ kb_api/Startup.cs
@@ -25,6 +25,7 @@ namespace kb_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.Add(new ServiceDescriptor(typeof(ILog), new MyConsoleLogger()));
             services.AddControllers();
         }
 
