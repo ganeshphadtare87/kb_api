32bacee3e2680a307d01fe4aa52ec53b132c4bd4
minakshi-md <minakshi-md@gmail.com>
2019-12-16 15:05:11 +0530

    Added Location Interface and class, LocationVm

---
+++ kb_bll/Services/ILocationService.cs
@@ -0,0 +1,14 @@
+﻿using kb_bll.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kb_bll.Services
+{
+    public interface ILocationService
+    {
+        List<LocationVM> GetLocations();
+    }
+}
---
+++ kb_bll/Services/LocationService.cs
@@ -0,0 +1,29 @@
+﻿using kb_bll.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using kb_bll.Models;
+
+namespace kb_bll.Services
+{
+    public class LocationService : ILocationService
+    {
+        KharidoBechoContext db = new KharidoBechoContext();
+        public List<LocationVM> GetLocations()
+        {
+            var locationList = (from l in db.Location
+                                select l).ToList();
+
+            List<LocationVM> locList = new List<LocationVM>();
+            //foreach (var loc in locationList) {
+
+            //    locList.Add(loc);
+            //}
+
+            return locList;
+        }
+    }
+}
